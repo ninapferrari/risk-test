240146953e4fd6e05753461e7a76ba1208d8d1d2
Nina Ferrari <ninapferrari@gmail.com>
2024-03-10 12:27:40 -0300

    feat: implement verify_transaction

--- src/populate_db.py
+++ src/populate_db.py
@@ -1,11 +1,11 @@
-import db
-import pandas as pd
+from db import conn
+import pandas
 from psycopg2.extras import execute_values
 
 transactions_file = '../data/transactional-sample.csv'
-df = pd.read_csv(transactions_file, dtype={'device_id': str})
+df = pandas.read_csv(transactions_file, dtype={'device_id': str})
 
-cursor = db.conn.cursor()
+cursor = conn.cursor()
 create_table_query = """
 CREATE TABLE IF NOT EXISTS transactions (
     transaction_id VARCHAR(10) PRIMARY KEY,
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS transactions (
     user_id VARCHAR(10),
     card_number VARCHAR(20),
     transaction_date TIMESTAMP,
-    transaction_amount NUMERIC,
-    device_id  VARCHAR(20),
+    transaction_amount NUMERIC(10, 2),
+    device_id  VARCHAR(10),
     has_cbk BOOLEAN
 );
 """
 
 cursor.execute(create_table_query)
-db.conn.commit()
+conn.commit()
 
 tuples = [tuple(x) for x in df.to_numpy()]
 
@@ -30,7 +30,7 @@ INSERT INTO transactions (transaction_id, merchant_id, user_id, card_number, tra
 """
 
 execute_values(cursor, insert_query, tuples)
-db.conn.commit()
+conn.commit()
 
 cursor.close()
-db.conn.close()
+conn.close()
